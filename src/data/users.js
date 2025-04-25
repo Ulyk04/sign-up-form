7a014919fd241048026b4b273ea5081934080ec6
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-04-25 15:51:40 +0300

    Update users.js

--- src/data/users.js
+++ src/data/users.js
@@ -1,15 +1,27 @@
 // src/data/users.js
 
+import { Password } from "@mui/icons-material";
+
 const users = [];
+const passwords =[];
 
 export function addUser(user) {
   users.push(user);
+}
+
+export function addPassword(password){ 
+  passwords.push(password)
 }
 
 export function getUserByEmail(email) {
   return users.find((u) => u.email === email);
 }
 
+export function getUserByPassword(pass){
+  return passwords.find((u) => u.password === pass);
+}
+
 export function getAllUsers() {
   return users;
 }
+
