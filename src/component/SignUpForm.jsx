e87193155ff1cbba7081d9300d8ee76c54e02e17
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-04-25 15:52:08 +0300

    Update SignUpForm.jsx

--- src/component/SignUpForm.jsx
+++ src/component/SignUpForm.jsx
@@ -4,8 +4,10 @@ import { AppProvider } from '@toolpad/core/AppProvider';
 import { SignInPage } from '@toolpad/core/SignInPage';
 import { useTheme } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
-import { addUser } from '../data/users'; // Импорт функции для сохранения
+import { addUser } from '../data/users'; 
+import { addPassword } from '../data/users';
 import { v4 as uuidv4 } from 'uuid';
+import { Box, Button } from '@mui/material';
 
 const providers = [{ id: 'credentials', name: 'Email and Password' }];
 
@@ -15,38 +17,45 @@ export default function SignUpForm() {
 
   return (
     <AppProvider theme={theme}>
-      <SignInPage
-        signIn={(provider, formData) => {
-          const email = formData.get('email');
-          const password = formData.get('password');
-          const tandc = formData.get('tandc');
+      <Box display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
+        <SignInPage
+          signIn={(provider, formData) => {
+            const email = formData.get('email');
+            const password = formData.get('password');
+            const tandc = formData.get('tandc');
 
-          if (email && password && tandc) {
-            addUser({ id: uuidv4(), email, password });
-            navigate('/hello');
-          } else {
-            alert('Please fill all fields and accept the terms.');
-          }
-        }}
-        slotProps={{
-          emailField: { variant: 'standard', autoFocus: false },
-          passwordField: { variant: 'standard' },
-          submitButton: { variant: 'outlined' },
-          rememberMe: {
-            control: (
-              <Checkbox
-                name="tandc"
-                value="true"
-                color="primary"
-                sx={{ padding: 0.5, '& .MuiSvgIcon-root': { fontSize: 20 } }}
-              />
-            ),
-            color: 'textSecondary',
-            label: 'I agree with the T&C',
-          },
-        }}
-        providers={providers}
-      />
+            if (email && password && tandc) {
+              addUser({email});
+              addPassword({password})
+              navigate('/hello');
+            } else {
+              alert('Please fill all fields and accept the terms.');
+            }
+          }}
+          slotProps={{
+            emailField: { variant: 'standard', autoFocus: false },
+            passwordField: { variant: 'standard' },
+            submitButton: { variant: 'outlined' },
+            rememberMe: {
+              control: (
+                <Checkbox
+                  name="tandc"
+                  value="true"
+                  color="primary"
+                  sx={{ padding: 0.5, '& .MuiSvgIcon-root': { fontSize: 20 } }}
+                />
+              ),
+              color: 'textSecondary',
+              label: 'I agree with the T&C',
+            },
+          }}
+          providers={providers}
+        />
+
+        <Button  sx={{marginTop: '-15%'}} variant="text" onClick={() => navigate('/login')}>
+          You already have a account? Login
+        </Button>
+      </Box>
     </AppProvider>
   );
 }
